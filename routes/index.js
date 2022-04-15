0d877ec6a87b27df4d54d6ae95bc07e74be4e524
YUEQIN18 <qinyue12345@gmail.com>
2022-04-15 15:11:12 -0400

    update app

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,13 @@ const comment = require('./comment');
 const constructorMethod = (app) => {
   // app.uses go here
   app.use('/', home);
-  app.use('/signup', signup);
-  app.use('/login', login);
-  app.use('/logout', logout);
-  app.use('/users', users);
-  app.use('/parks', parks);
-  app.use('/appointments', appointments);
-  app.use('/notification', notification);
+  // app.use('/signup', signup);
+  // app.use('/login', login);
+  // app.use('/logout', logout);
+  // app.use('/users', users);
+  // app.use('/parks', parks);
+  // app.use('/appointments', appointments);
+  // app.use('/notification', notification);
   
   app.all('*', (req, res) => {
     res.status(404).json('Error 404: Site path not found');
