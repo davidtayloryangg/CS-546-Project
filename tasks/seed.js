5420c1b4110e3c7900272f391603c3dc4f72cd09
David Yang <David@Davids-MacBook-Pro-3.local>
2022-04-24 21:49:54 -0400

    adding parks and some activites

--- tasks/seed.js
+++ tasks/seed.js
@@ -72,6 +72,79 @@ async function test() {
   //   console.error(e);
   // }
 
+  try {
+    //Parks
+    const columbus = await parks.createPark(
+      "Columbus Park",
+      "06:00",
+      "22:00",
+      "900 Clinton St, Hoboken, NJ 07030"
+    ); //Activities: Playground, Tennis, Basketball
+    console.log(columbus._id + " : " + columbus._id);
+    const playgroundColumbus = await activities.createActivity(
+      columbus._id.toString(),
+      "Playground Columbus Park",
+      "1",
+      "20"
+    );
+    const TennisColumbus = await activities.createActivity(
+        columbus._id.toString(),
+        "Tennis Columbus Park",
+        "2",
+        "4"
+    );
+    const BasketballColumbus = await activities.createActivity(
+      columbus._id.toString(),
+      "Basketball Columbus Park",
+      "1",
+      "10"
+  );
+    const churchSquare = await parks.createPark(
+      "Church Square Park",
+      "06:00",
+      "23:00",
+      "400 Garden St, Hoboken, NJ 07030"
+    ); //Activities: Dog Park, Basketball
+    const madison = await parks.createPark(
+      "Madison Park",
+      "06:00",
+      "22:00",
+      "305 Monroe St, Hoboken, NJ 07030"
+    ); //Activities: Playground
+    const sinatra = await parks.createPark(
+      "Sinatra Park",
+      "00:01",
+      "00:00",
+      "500 Frank Sinatra Dr, Hoboken, NJ 07030"
+    ); //Activities: Soccer
+    const stevens = await parks.createPark(
+      "Stevens Park",
+      "08:00",
+      "22:00",
+      "401 Hudson St, Hoboken, NJ 07030"
+    ); //Activities: Baseball
+    const CP = await parks.createPark(
+      "Castle Point Skatepark",
+      "00:01",
+      "00:00",
+      "9 Castle Point Terrace, Hoboken, NJ 07030"
+    ); //Activities: Skate
+    const PC = await parks.createPark(
+      "Pier C Park",
+      "08:00",
+      "22:00",
+      "340 Sinatra Dr, Hoboken, NJ 07030"
+    ); //Activities: Playground
+    const PA = await parks.createPark(
+      "1600 Park",
+      "07:00",
+      "22:00",
+      "340 Sinatra Dr, Hoboken, NJ 07030"
+    ); //Activities: Soccer
+  } catch (e) {
+    console.log(e);
+  }
+
   await dbConnection.closeConnection();
 }
 
