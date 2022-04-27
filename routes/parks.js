35f40b42bc565d298391fcc7ee0a864b606e9712
David <davidtayloryang@gmail.com>
2022-04-27 17:47:31 -0400

    ParkList and SinglePark Page Handlebars. Parks route for /parks and /parks/id

--- routes/parks.js
+++ routes/parks.js
@@ -1,28 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data/parks');
+const data = require("../data/parks");
 
-router
-  .route('/AllParks')
-  .get(async (req, res) => {
-    try {
-      const allParks = await data.getAllParks();
-      res.json(allParks);
-    } catch (e) {
-      res.status(500).json(e);
+router.get("/", function (req, res) {
+  data.getAllParks().then(
+    (parks) => {
+      res.render("function/Park List", { parks: parks });
+    },
+    (error) => {
+      res.status(500).json({ error: error });
     }
-  })
+  );
+});
 
-router
-  .route('/search')
-  .post(async (req, res) => {
-    try {
-      const info = req.body;
-      const searchParks = await data.getParksByName(info.parkName);
-      res.json(searchParks);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  })
+router.route("/:id").get(async (req, res) => {
+  try {
+    const parks = await data.getParkById(req.params.id);
+    res.render("function/SinglePark", { parks: parks });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// router.route("/:id").post(async (req, res) => {
+//   data.getParkById(req.params.id).then(
+//     (parks) => {
+//       res.render("function/SinglePark", { parks: parks });
+//     },
+//     (error) => {
+//       res.status(500).json({ error: error });
+//     }
+//   );
+// });
+
+router.route("/AllParks").get(async (req, res) => {
+  try {
+    const allParks = await data.getAllParks();
+    res.json(allParks);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.route("/search").post(async (req, res) => {
+  try {
+    const info = req.body;
+    const searchParks = await data.getParksByName(info.parkName);
+    res.json(searchParks);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 module.exports = router;
