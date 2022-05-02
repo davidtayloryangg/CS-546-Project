981546b05538da026ea6adbf4fd54a21952c6a82
David <davidtayloryang@gmail.com>
2022-05-01 23:07:53 -0400

    Edit park on park list page redirect to edited park page, Create new park form redirect to new park page

--- routes/parks.js
+++ routes/parks.js
@@ -52,12 +52,70 @@ router.route("/id/:id").get(async (req, res) => {
 });
 
 router
+  .route("/id/:id/edit")
+  .get(async (req, res) => {
+    if (!req.session.user) {
+      res.redirect("/users");
+    } else {
+      try {
+        const park = await data.getParkById(req.params.id);
+        res.render("function/EditPark", { parks: park });
+      } catch (error) {
+        res.status(500).json({ error: error });
+      }
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const updatedParkInfo = req.body;
+      console.log(updatedParkInfo);
+      const { id, name, openTime, closeTime, location } = updatedParkInfo;
+      const updatedPark = await data.updatePark(
+        id,
+        name,
+        openTime,
+        closeTime,
+        location
+      );
+      res.redirect(`/parks/id/${id}`);
+    } catch (e) {
+      res.status(500).json({ error: e });
+    }
+  });
+
+router
+  .route("/CreatePark")
+  .get(async (req, res) => {
+    if (!req.session.user) {
+      res.redirect("/users");
+    } else {
+      res.render("function/CreatePark");
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      // console.log(req.body);
+      const newParkInfo = req.body;
+      const { name, openTime, closeTime, location } = newParkInfo;
+      // console.log(name, openTime, closeTime, location);
+      const newParkId = await data.createPark(
+        name,
+        openTime,
+        closeTime,
+        location
+      );
+      res.redirect("/parks/id/" + newParkId._id);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  });
+
+router
   .route("/id/comments/:id")
   .get(async (req, res) => {
     if (req.session && req.session.user)
       var currentUsername = req.session.user.name;
-    else
-      currentUsername = null;
+    else currentUsername = null;
     try {
       const park = await data.getParkById(req.params.id);
       const comments = park.comments;
@@ -70,8 +128,8 @@ router
           userId: element.userId,
           username: name,
           comment: element.parkComment,
-          timestamp: element.timestamp
-        }
+          timestamp: element.timestamp,
+        };
         userList.push(user);
       }
       res.json(userList);
@@ -85,12 +143,17 @@ router
         const userInfo = req.session.user;
         const info = req.body;
         const parkId = req.params.id;
-        await commentdata.createComment(parkId, userInfo.userId, info.newCommentRating, info.newCommentTxt);
+        await commentdata.createComment(
+          parkId,
+          userInfo.userId,
+          info.newCommentRating,
+          info.newCommentTxt
+        );
       } catch (error) {
         res.status(500).json({ error: error });
       }
-    }
-    else res.render('function/Login', { error: "Log in to comment parks!!!" });
-  })
+    } else
+      res.render("function/Login", { error: "Log in to comment parks!!!" });
+  });
 
 module.exports = router;
