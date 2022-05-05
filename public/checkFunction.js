5f680a356eb605a70a1243feacf7d549e4ed62f7
YUEQIN18 <qinyue12345@gmail.com>
2022-05-05 00:49:41 -0400

    check

--- public/checkFunction.js
+++ public/checkFunction.js
@@ -1,10 +1,10 @@
 function checkEmail() {
-  var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/; //正则表达式
-  var obj = document.getElementById("email"); //要验证的对象
-  if (obj.value === "") { //输入不能为空
+  var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
+  var obj = document.getElementById("email");
+  if (obj.value === "") {
     $("#inputError").text("Error: Email must be not empty");
     return false;
-  } else if (!reg.test(obj.value)) { //正则验证不通过，格式不对
+  } else if (!reg.test(obj.value)) {
     $("#inputError").text("Error: invalid email format")
     return false;
   } else {
