b57079d4c03de65b208a1debd30a9ae29084b33c
Yuheng Xiao <qq756951570@gmail.com>
2022-05-06 10:43:25 -0400

    Added errorchecking on the routers of users_signup and users_porfile_update

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const userData = require('../data/users');
 const parkData = require('../data/parks');
 const activityData = require('../data/activities');
 const appointmentData=require('../data/appointments');
+const func = require('../data/functions');
 var xss = require("xss");
 
 
@@ -59,6 +60,10 @@ router.post('/signup', async (req, res) => {
     const lastname = xss(body.lastname);
     const email = xss(body.email);
     const password = xss(body.password);
+    func.checkUserName(firstname);
+    func.checkUserName(lastname);
+    func.checkEmail(email);
+    func.checkPassword(password);
     // if (!firstname || !lastname || !email || !password)
     //   throw "must provide all inputs";
     let x = await userData.createUser(firstname, lastname, email, password)
@@ -73,67 +78,30 @@ router.post('/signup', async (req, res) => {
 
 router.get('/profile', async (req, res) => {
   if (req.session && req.session.user) {
-    const userInfo = req.session.user;
-    const user = await userData.getUserByEmail(userInfo.email);
-    let park;
-    let activity;
-    let date;
-    let appointment;
-    let appointments = []
-    for (let i = 0; i < user.appointments.length; i++) {
-      appointment=await appointmentData.getAppointmentById(user.appointments[i].toString())
-      park=await parkData.getParkById(appointment.parkId.toString())
-      activity=await activityData.get(appointment.activityId.toString())
-      date = appointment.month + "/" + appointment.day + "/" + appointment.year
-      appointmentinfo = {
-        park: park.name,
-        activity: activity.name,
-        date: date,
-        time: appointment.hour,
-        status: appointment.status
-      }
-      appointments.push(appointmentinfo)
-    };
-    let userinfo = {
-      id: user._id.toString(),
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: user.email,
-      gender: user.gender,
-      city: user.city,
-      state: user.state,
-      age: user.age,
-      description: user.description,
-      favorites: user.favorites,
-      appointments: appointments
-    }
-    res.status(200).render('function/UserProfile', { user: userinfo, edit: false });
-  } else {
-    res.status(500).redirect('/users/login')
-  }
-});
-
-router.post('/profile', async (req, res) => {
-  if (req.session && req.session.user) {
-    var body = req.body;
-    // if (!body.id || !body.email || !body.gender || !body.city || !body.state || !body.age || !body.description) throw "Please provide all input for modifyUserProfile!";
-    if (Object.keys(body).length != 1) {
-      const id = xss(body.id);
-      const email = xss(body.email);
-      const gender = xss(body.gender);
-      const city = xss(body.city);
-      const state = xss(body.state);
-      const age = xss(body.age);
-      const description = xss(body.description);
-      const updated = await userData.modifyUserProfile(id, email, gender, city, state, age, description);
-      if (updated) {
-        res.status(200).redirect("/users/profile")
-      }
-    } else {
-      const user = await userData.getUserById(body.id)
-      const bodyid = xss(body.id);
+    try {
+      const userInfo = req.session.user;
+      const user = await userData.getUserByEmail(userInfo.email);
+      let park;
+      let activity;
+      let date;
+      let appointment;
+      let appointments = []
+      for (let i = 0; i < user.appointments.length; i++) {
+        appointment=await appointmentData.getAppointmentById(user.appointments[i].toString())
+        park=await parkData.getParkById(appointment.parkId.toString())
+        activity=await activityData.get(appointment.activityId.toString())
+        date = appointment.month + "/" + appointment.day + "/" + appointment.year
+        appointmentinfo = {
+          park: park.name,
+          activity: activity.name,
+          date: date,
+          time: appointment.hour,
+          status: appointment.status
+        }
+        appointments.push(appointmentinfo)
+      };
       let userinfo = {
-        id: bodyid,
+        id: user._id.toString(),
         firstname: user.firstname,
         lastname: user.lastname,
         email: user.email,
@@ -143,9 +111,55 @@ router.post('/profile', async (req, res) => {
         age: user.age,
         description: user.description,
         favorites: user.favorites,
-        appointments: user.appointments
+        appointments: appointments
       }
-      res.status(200).render('function/UserProfile', { user: userinfo, edit: true })
+      res.status(200).render('function/UserProfile', { user: userinfo, edit: false });
+    } catch (e) {
+      res.status(500).render('function/Appointment_Error', { error: e, title: "Error" });
+    }
+  } else {
+    res.status(500).redirect('/users/login')
+  }
+});
+
+router.post('/profile', async (req, res) => {
+  if (req.session && req.session.user) {
+    try {
+      var body = req.body;
+      // if (!body.id || !body.email || !body.gender || !body.city || !body.state || !body.age || !body.description) throw "Please provide all input for modifyUserProfile!";
+      if (Object.keys(body).length != 1) {
+        const id = xss(body.id);
+        const email = xss(body.email);
+        const gender = xss(body.gender);
+        const city = xss(body.city);
+        const state = xss(body.state);
+        const age = xss(body.age);
+        const description = xss(body.description);
+        func.checkEmail(email)
+        const updated = await userData.modifyUserProfile(id, email, gender, city, state, age, description);
+        if (updated) {
+          res.status(200).redirect("/users/profile")
+        }
+      } else {
+        const user = await userData.getUserById(body.id)
+        const bodyid = xss(body.id);
+        let userinfo = {
+          id: bodyid,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          gender: user.gender,
+          city: user.city,
+          state: user.state,
+          age: user.age,
+          description: user.description,
+          favorites: user.favorites,
+          appointments: user.appointments
+        }
+        res.status(200).render('function/UserProfile', { user: userinfo, edit: true })
+      }
+    } catch (e) {
+      res.status(500).render('function/Appointment_Error', { error: e, title: "Error" });
     }
   } else {
     res.status(400).redirect('/users/login')
